4faca2a946592abc387ac39a5521c77fc32d8fc2
nikitashalabaev <nikitashdev@gmail.com>
2021-06-01 19:19:58 +0300

    Updates

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   products: 'https://sdyaa5isjk.execute-api.eu-west-1.amazonaws.com/dev/products',
   // order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  // import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ayk8rtu69b.execute-api.eu-west-1.amazonaws.com/dev/import',
   // bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   // cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
